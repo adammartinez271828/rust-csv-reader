606c82945f9c97e0a97540618c09dd90267c34df
Adam Martinez <adam.paul.martinez@gmail.com>
2017-03-26 06:51:24 -0400

    Implement basic file read

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::path::Path;
+
+const PATH_STR: &'static str = "/home/adam/devel/rust/csv_reader/abalone.data";
+
+struct AbaloneResult {}
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new(PATH_STR);
+    let display = path.display();
+
+    let file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why.description()),
+        Ok(file) => file,
+    };
+    let mut reader = BufReader::new(file);
+    let mut buffer = String::new();
+
+    // read a line into buffer
+    reader.read_line(&mut buffer);
+
+    println!("{}", buffer);
 }
